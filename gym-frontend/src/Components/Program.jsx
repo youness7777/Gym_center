a87f567e38c94534498b27dd4c848cc2d5a5ef2b
youness7777 <57494358+youness7777@users.noreply.github.com>
2024-12-26 01:06:44 +0000

    setting css like the maquette for program section

--- gym-frontend/src/Components/Program.jsx
+++ gym-frontend/src/Components/Program.jsx
@@ -1,39 +1,38 @@
 import React, { useRef } from 'react'
 import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
 import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import cardio from '../Assets/cardio_icon.png';
+import fat from '../Assets/fat_icon.png';
+import muscles from '../Assets/muscles_icon.png';
+import nutritions from '../Assets/nutritions_icon.png';
 
 export const Program = () => {
     const programs = [
         {
           title: "Cardio Strength",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
-          icon: "❤️",
+          icon:cardio,
           isActive: true,
         },
         {
           title: "Fat Lose",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
-          icon: "🦴",
+          icon:fat,
           isActive: false,
         },
         {
           title: "Muscle Gain",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
-          icon: "💪",
+          icon: muscles,
           isActive: false,
         },
         {
           title: "Nutrition Plan",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
-          icon: "🥗",
+          icon: nutritions,
           isActive: false,
         },
-        {
-            title: "Nutrition Plan",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
-            icon: "🥗",
-            isActive: false,
-        },
+       
       ];
 
         const containerRef = useRef(null);
@@ -47,9 +46,9 @@ export const Program = () => {
         };
       
   return (
-    <div className='relative text-white bg-[#090707] w-full h-auto text-left   py-24 md:flex-row'>
-        <div className='flex justify-between px-24'>
-            <div className=''>
+    <div className='relative text-white bg-[#090707] w-full h-auto text-left px-[120px]   py-[80px] md:flex-row'>
+        <div className='flex justify-between '>
+            <div>
                 <p className=' text-[56px] font-bold '>Explore Our Programs</p>
                 <p className='absolute text-[142px] text-white opacity-10 font-bold top-0 left-0 '>Programs</p>
             </div>
@@ -66,16 +65,18 @@ export const Program = () => {
         </div>
 
         <div className='max-w-screen-xl'>
-            <div ref={containerRef} className='flex gap-[26px]  mb-[104px]  mt-[93px] overflow-x-hidden overflow-y-hidden'>
+            <div ref={containerRef} className='flex gap-[26px]  mb-[104px]  mt-[93px] overflow-x-hidden overflow-y-hidden '>
                 {programs.map((program, index) => (
                 <div
                 key={index}
-                className='flex-shrink-0 w-[320px] h-[240px]  items-center py-10 px-6  rounded-xl bg-[#0D0D0D] hover:bg-[#097FD9] scale-105 duration-300' 
+                className='flex-shrink-0  w-[320px] h-[240px]  items-center py-[40px] px-[24px]  rounded-xl bg-[#0D0D0D] hover:bg-[#097FD9] scale-105 duration-300' 
               
                 >
-                <div className="text-4xl mb-4">{program.icon}</div>
-                <h3 className="text-white text-xl font-semibold font-poppins text-[24px] leading-[24px] mb-2">{program.title}</h3>
-                <p className="text-white/70 text-sm">{program.description}</p>
+                <div className="w-[32px] h-[32px] mb-[24px]">
+                  <img src={program.icon}/>
+                </div>
+                <h3 className="text-white text-xl font-semibold font-poppins text-[24px] leading-[24px] pb-[8px]">{program.title}</h3>
+                <p className="text-white/50 text-[16px]">{program.description}</p>
                 </div>
                ))}
 
